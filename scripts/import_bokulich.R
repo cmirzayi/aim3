6130ff050e2c187dbba18823ca085c26b370b986
cmirzayi <cmirzayi@gmail.com>
2022-03-18 17:31:50 -0400

    import bokulich data

---
+++ scripts/import_bokulich.R
@@ -0,0 +1,19 @@
+# Prepare Bokulich dataset
+
+# Load necessary packages
+library(phyloseq)
+library(dplyr)
+
+readbioms <- function(x){
+  ps <- paste0("data/bokulich/bioms/BIOM/", x, "/otu_table.biom") %>% import_biom
+  sam <- paste0("data/bokulich/bioms/mapping_files/", x, "_mapping_file.txt") %>%
+    read.table(., sep="\t", header = TRUE, row.names =1)
+  sample_data(ps) <- sam
+  return(ps)
+}
+
+samples <- c("3810", "3811", "3812", "3813", "3814")
+lps <- lapply(samples, readbioms)
+
+merged <- merge_phyloseq(lps[[1]], lps[[2]], lps[[3]], lps[[4]], lps[[5]])
+sample_data(merged)
